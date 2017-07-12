e51c2f847d4d9e78fb25bfb8a881e10c152fa360
naoki ohshima <saylako@icloud.com>
2017-07-12 11:56:19 +0000

    gem3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,7 +54,7 @@ class PostsController < ApplicationController
 
  private
    def posts_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :image, :image_cache, :remove_image)
    end
 
    def set_post
