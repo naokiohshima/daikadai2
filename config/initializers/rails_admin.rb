cb8a92ead0f23c708e998c980a44ef5f3dfd0299
naoki ohshima <saylako@icloud.com>
2017-07-22 07:46:24 +0000

    admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,8 +3,8 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-   config.authenticate_with do
-     warden.authenticate! scope: :user
+  config.authenticate_with do
+    warden.authenticate! scope: :user
    end
    config.current_user_method(&:current_user)
 
